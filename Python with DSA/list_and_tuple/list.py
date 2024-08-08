e8e43e7aa9a9b60173767ddad73ed346d73d3e76
Muhammad Hisham <hisham.cs.engineer@gmail.com>
2024-08-08 19:51:15 +0530

    Python with Dsa

---
+++ Python with DSA/list_and_tuple/list.py
@@ -0,0 +1,24 @@
+# What is list in python
+# List is a collection which is ordered and changeable. Allows duplicate members.
+
+list1= [1,2,3,4,5,6,7,8,9,10]
+
+
+# list1=list()
+print(list1)
+print(list)
+
+list2 = [1,2,.3,"hi",True]
+# list can  hold different data types 
+
+list2[0]= 100
+# list are ordered elements
+print(list2)
+#list can accessed by index
+#index can be negative
+print(list2[0])
+# list are mutable
+list2.append(800)
+print(list2)
+
+#list are dynamically sized
---
+++ Python with DSA/list_and_tuple/tuple.py
@@ -0,0 +1,3 @@
+# what is tuple in python
+
+# Tuple is a collection of data which is ordered and unchangeable. It is written with round brackets. It can have any number of items and they may be of different types (integer, float, string etc.). It is similar to a list, but it is immutable, which means that once a tuple is created, its elements cannot be modified.
