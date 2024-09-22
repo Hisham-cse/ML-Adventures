97c952c8fc0dd6e283b0e906f79fcd706b5e3a27
Muhammad Hisham <hisham.cs.engineer@gmail.com>
2024-09-22 23:49:38 +0530

    Functions,modules

---
+++ importing modules in python/package/__init__.py
@@ -0,0 +1,3 @@
+''' init.py is a special file used in Python to define
+packages and initialize their namespaces '''
+
---
+++ importing modules in python/test.py
@@ -0,0 +1,7 @@
+from package.maths import *
+from package.subpackages.mult import multiply
+
+print(addition(2,3))
+print(sub(2,3))
+print(sub(6,3))
+print(multiply(6,3))
